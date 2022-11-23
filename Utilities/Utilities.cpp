734dbb0e5fa745b9b14977590f5776ec89b0757c
Endrophes <Endrophes@users.noreply.github.com>
2022-11-23 11:15:58 -0500

    Update Utilities.cpp
    Clean up template comments.
    
    Working out a way to test stopwatch timings.

--- Utilities/Utilities.cpp
+++ Utilities/Utilities.cpp
@@ -1,31 +1,19 @@
 // Utilities.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <stdlib.h>
 
 #include "Lib/StopwatchService.h";
 
 int main()
 {
-    //std::cout << "Hello World!\n";
-
     stopwatchService.startWatch("THIS IS A TEST");
 
 
     //DO Stuff
+    std::cin.ignore(); // a pause for a user defined time to test stopwatch
 
 
     stopwatchService.endWatch("THIS IS A TEST");
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
